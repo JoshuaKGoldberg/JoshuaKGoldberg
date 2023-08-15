7fbf10c7c326e4023749e44f01447a23b1da912f
Josh Goldberg <git@joshuakgoldberg.com>
2023-08-15 00:26:18 -0400

    fix: instead of exporting functions, just ... don't

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,4 @@ import { projectCategories } from "./projects.js";
 
 export { projectCategories };
 
-export { add } from "./add/index.js";
-export { generate } from "./generate/index.js";
 export type * from "./types.js";
